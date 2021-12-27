08edf3d7dfd23c38e0b5a0611093a1be8ced077d
Ankit Raushan <raushankit160226@gmail.com>
2021-12-27 20:36:47 +0530

    added codes

---
+++ src/classes/pageCode.cpp
@@ -0,0 +1,69 @@
+#include "pageCode.h"
+
+void ProfilePageCodes()
+{
+    std::cout << "========================" << std::endl;
+    std::cout << "Keycode\toperation" << std::endl;
+    std::cout << "-------\t---------" << std::endl;
+    std::cout << "   C   \tView Cart" << std::endl;
+    std::cout << "   W   \tView Wishlist" << std::endl;
+    std::cout << "   O   \tView orders" << std::endl;
+    std::cout << "   B   \tBack" << std::endl;
+    std::cout << "   S   \tSearch Products" << std::endl;
+    std::cout << "   A   \tAdd address" << std::endl;
+    std::cout << "   E   \tExit" << std::endl;
+    std::cout << "========================\n"
+              << std::endl;
+}
+
+void ProductPageCodes()
+{
+    std::cout << "========================" << std::endl;
+    std::cout << "Keycode\toperation" << std::endl;
+    std::cout << "-------\t---------" << std::endl;
+    std::cout << "   A   \tAdd to Cart" << std::endl;
+    std::cout << "   P   \tBuy Now" << std::endl;
+    std::cout << "   W   \tAdd to Wishlist" << std::endl;
+    std::cout << "   O   \torders" << std::endl;
+    std::cout << "   B   \tBack" << std::endl;
+    std::cout << "   R   \trate the product" << std::endl;
+    std::cout << "   V   \tWrite a review" << std::endl;
+    std::cout << "   S   \tSearch Products" << std::endl;
+    std::cout << "   E   \tExit" << std::endl;
+    std::cout << "   L n \tLike n-th review" << std::endl;
+    std::cout << "   D n \tDislike n-th review" << std::endl;
+    std::cout << "========================\n"
+              << std::endl;
+}
+
+void frontPageCodes()
+{
+    std::cout << "========================" << std::endl;
+    std::cout << "Keycode\toperation" << std::endl;
+    std::cout << "-------\t---------" << std::endl;
+    std::cout << "   S   \tSearch" << std::endl;
+    std::cout << "   P   \tProfile" << std::endl;
+    std::cout << "   C   \tCart" << std::endl;
+    std::cout << "   W   \tWishlist" << std::endl;
+    std::cout << "   O   \torders" << std::endl;
+    std::cout << "   E   \tExit" << std::endl;
+    std::cout << "========================\n"
+              << std::endl;
+}
+
+void cartPageCodes()
+{
+    std::cout << "========================" << std::endl;
+    std::cout << "Keycode\toperation" << std::endl;
+    std::cout << "-------\t---------" << std::endl;
+    std::cout << "   S   \tSearch" << std::endl;
+    std::cout << "   P   \tProfile" << std::endl;
+    std::cout << "   C   \tCheckout" << std::endl;
+    std::cout << "   R   \tRemove item" << std::endl;
+    std::cout << "   W   \tWishlist" << std::endl;
+    std::cout << "   O   \tOrders" << std::endl;
+    std::cout << "   B   \tBack" << std::endl;
+    std::cout << "   E   \tExit" << std::endl;
+    std::cout << "========================\n"
+              << std::endl;
+}
---
+++ src/classes/pageCode.h
@@ -0,0 +1,14 @@
+#ifndef __PAGECODE__
+#define __PAGECODE__
+
+#include <iostream>
+
+void ProfilePageCodes();
+
+void ProductPageCodes();
+
+void frontPageCodes();
+
+void cartPageCodes();
+
+#endif

70f5259fa184018fbe782a54480ed47e172a09db
Ankit Raushan <raushankit160226@gmail.com>
2021-12-27 21:10:25 +0530

    improved order class

--- src/classes/order.cpp
+++ src/classes/order.cpp
@@ -1,18 +1,22 @@
 #include "order.h"
 
-order::order(){
+order::order()
+{
     isDelivered = false;
     log.clear();
 }
 
-order::order(const order &p){
+order::order(const order &p)
+{
     isDelivered = p.isDelivered;
     log = p.log;
     ad = p.ad;
 }
 
-order& order::operator=(const order &p){
-    if(this!=&p){
+order &order::operator=(const order &p)
+{
+    if (this != &p)
+    {
         isDelivered = p.isDelivered;
         log = p.log;
         ad = p.ad;
@@ -20,29 +24,36 @@ order& order::operator=(const order &p){
     return *this;
 }
 
-void order::changeStat(){
+void order::changeStat()
+{
     isDelivered = true;
 }
 
-ostream& operator<<(ostream &out, const order &p){
-    out << "Delivered: " << p.isDelivered << endl;
-    out << p.ad << endl;
-    out << "Logsize: " << p.log.size() << endl;
-    for(auto i:p.log) out << i << endl;
+std::ostream &operator<<(std::ostream &out, const order &p)
+{
+    out << "Delivered: " << p.isDelivered << std::endl;
+    out << p.ad << std::endl;
+    out << "Logsize: " << p.log.size() << std::endl;
+    for (auto i : p.log)
+        out << i << std::endl;
     return out;
 }
 
-istream& operator>>(istream &in, order &p){
-    string str;
-    getline(in,str);
-    stringstream ss(str);
+std::istream &operator>>(std::istream &in, order &p)
+{
+    std::string str;
+    getline(in, str);
+    std::stringstream ss(str);
     ss >> str >> p.isDelivered;
     int n;
     in >> p.ad;
     getline(in, str);
-    ss.str("");ss.clear();ss << str;
+    ss.str("");
+    ss.clear();
+    ss << str;
     ss >> str >> n;
-    for(int i = 0;i < n;i++){
+    for (int i = 0; i < n; i++)
+    {
         tracklog temp;
         in >> temp;
         p.log.push_back(temp);
@@ -50,6 +61,7 @@ istream& operator>>(istream &in, order &p){
     return in;
 }
 
-void order::addlog(string s){
+void order::addlog(std::string s)
+{
     log.push_back(tracklog(s));
 }
--- src/classes/order.h
+++ src/classes/order.h
@@ -5,21 +5,23 @@
 #include "tracklog.h"
 #include <vector>
 
-class order{
+class order
+{
+public:
+    order();
+    order(const order &);
+    order &operator=(const order &);
+    void changeStat();
+    void addlog(std::string);
+    friend std::ostream &operator<<(std::ostream &, const order &);
+    friend std::istream &operator>>(std::istream &, order &);
+
+private:
     bool isDelivered;
     address ad;
-    vector<tracklog> log;
+    std::vector<tracklog> log;
 
     friend class Kart;
-    public:
-    order();
-    order(const order&);
-    order& operator=(const order&);
-    void changeStat();
-    void addlog(string);
-    friend ostream& operator<<(ostream&, const order&);
-    friend istream& operator>>(istream&, order&);
 };
-
 
 #endif
